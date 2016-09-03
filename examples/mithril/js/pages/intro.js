8c86e1f779a2d838528d780581a3ebd76b5166d4
porsager <rasmus@porsager.com>
2016-09-03 19:54:44 +0200

    Added components to use for jail showcase

--- examples/mithril/js/pages/intro.js
+++ examples/mithril/js/pages/intro.js
@@ -1,10 +1,16 @@
 import m from 'mithril'
 
+const heading = {
+  view: vnode => m('h1', vnode.attrs.content )
+}
+
+const paragraph = {
+  view: vnode => m('p', vnode.attrs.content )
+}
+
 export default {
   view: () => [
-    m('h1', 'Introduction'),
-    m('p', `
-      So while you're listening to that great song you should go ahead and edit the files inside examples/mithril
-    `)
+    m(heading, { content: 'Introduction' }),
+    m(paragraph, { content: 'So while you\'re listening to that great song you should go ahead and edit the files inside examples/mithril' })
   ]
 }
