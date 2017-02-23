232b204693351257ceb8d1fe542838067083fdb0
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2017-02-23 15:04:40 +0200

    add window.onhashchange event (#9)

--- examples/mithril/js/app.js
+++ examples/mithril/js/app.js
@@ -4,12 +4,18 @@ import login from './models/login'
 
 window.m = m
 
-if (!login.user) {
-  if (window.location.pathname !== '/login')
-    login.redirect = window.location.pathname + window.location.search
-  window.history.pushState(null, null, '/login')
+let checkLogin = () => {
+  if (!login.user) {
+    console.warn('Unauthorized')
+    if (window.location.pathname !== '/login')
+      login.redirect = window.location.pathname + window.location.search
+    window.history.pushState(null, null, '/login')
+  }
 }
+
+window.onhashchange = () => checkLogin()
 
 window.onload = () => {
   m.route(document.body, '/404', routes)
+  checkLogin()
 }
